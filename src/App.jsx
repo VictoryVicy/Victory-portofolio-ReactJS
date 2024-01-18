9db659403e856445cd67fd7d7a049cb427b79b98
VictoryVicy <obed.noni99@gmail.com>
2024-01-18 17:36:26 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <>
+      <header>
+        <div className='container m-auto px-4 py-6'>
+          <div className='flex justify-between'>
+            <div>
+              <h1 className='font-bold text-xl'>Victory Portofolio</h1>
+            </div>
+            <div>
+              <ul className='flex gap-3'>
+                <li>
+                  <a className='text-gray-400 hover:text-white cursor-pointer'>Projects</a>
+                </li>
+                <li>
+                  <a className='text-gray-400 hover:text-white cursor-pointer'>Technologies</a>
+                </li>
+                <li>
+                  <a className='text-gray-400 hover:text-white cursor-pointer'>About me</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </header>
+      <main>
+        <section>
+          <div className='container m-auto px-4 py-10 flex'>
+            <div>
+              
+            </div>
+            <div>
+              
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  )
+}
