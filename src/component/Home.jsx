985ccc52b9a9a5f55bd8122f95c19b2a812bd692
VictoryVicy <obed.noni99@gmail.com>
2024-02-06 22:55:47 +0700

    Home #2

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -3,24 +3,31 @@ import Profile from "../assets/profile.jpg";
 export default function Home() {
   return (
     <section id="home" className="sm:mt-8 mt-12">
-      <div className="container m-auto px-4 pt-12 sm:py-32 flex flex-col sm:flex-row gap-16">
+      <div className="container m-auto px-12 pt-12 sm:py-32 flex flex-col sm:flex-row gap-16">
         <div>
-          <h1 className="font-bold text-4xl text-white">
+          <h1 className="font-bold text-4xl text-white mt-20">
             Hello, I am Victory,
           </h1>
           <h1 className="font-bold text-4xl mt-1 gradient-text running-text">
             fullstack developer.
           </h1>
-          <p className="mt-4 text-gray-400">
-            Informatics student with web development skills, learning more about
-            machine learning and data mining.
+          <p className="mt-4 text-gray-400 mt-8">
+            I am a passionate Informatics student. I am currently in my 4th
+            semester and am aiming to become an expert software developer.
+            Although I do not have any official work experience, I am very
+            enthusiastic about IT-related jobs. Currently, I am deepening my
+            understanding of machine learning, especially in the field of
+            natural language processing (NLP). I believe my skills in web
+            development and deep understanding of machine learning will give me
+            the opportunity to participate in future challenges in the world of
+            technology.
           </p>
         </div>
         <div className="relative">
           <img
             src={Profile}
             alt="Victory"
-            className="rounded-full relative w-[200px] m-auto sm:w-[400px]"
+            className="rounded-full relative w-[300px] m-auto sm:w-[2000px]"
           />
         </div>
       </div>
