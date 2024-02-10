9ae60d1d7032b0693966ac48dd5d03e21dab77e3
VictoryVicy <obed.noni99@gmail.com>
2024-02-10 21:29:10 +0700

    Homepage teros

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -3,8 +3,8 @@ import Profile from "../assets/profile.jpg";
 export default function Home() {
   return (
     <section id="home" className="sm:mt-8 mt-12">
-      <div className="container m-auto px-12 pt-12 sm:py-32 flex flex-col sm:flex-row gap-16 grid grid-cols-12">
-        <div className="md:col-span-6">
+      <div className="container m-auto px-4 sm:px-6 lg:px-8 pt-12 sm:py-32 flex flex-col-reverse sm:flex-row sm:gap-4 gap-8">
+        <div className="sm:col-span-6">
           <h1 className="font-bold text-4xl text-white mt-20">
             Hello, I am Victory,
           </h1>
@@ -20,14 +20,14 @@ export default function Home() {
             natural language processing (NLP). I believe my skills in web
             development and deep understanding of machine learning will give me
             the opportunity to participate in future challenges in the world of
-            technology.a
+            technology.
           </p>
         </div>
-        <div className="relative ml-20 md:col-span-6">
+        <div className="relative sm:col-span-6">
           <img
             src={Profile}
             alt="Victory"
-            className="rounded-full relative w-[200px] m-auto sm:w-[400px]"
+            className="rounded-full w-full sm:w-auto md:max-w-[400px] mx-auto"
           />
         </div>
       </div>
